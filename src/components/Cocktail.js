c5199bf35a9a8391d10a68d46819b28781bf3f61
KIRILL F <tiltlessman@gmail.com>
2022-03-05 14:10:20 +0300

    update styling navbar and drinks list

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -13,8 +13,22 @@ const Cocktail = ({ idDrink: id, strDrinkThumb: img, strDrink: name }) => {
 const Wrapper = styled.article`
   width: 200px;
   height: 200px;
+  border: 2px solid black;
+  border-radius: 6px;
   img {
     width: 100%;
+  }
+  h5 {
+    transform: translateY(-120%);
+    padding: 10px 10px 10px 10px;
+    margin-top: 25px;
+    margin-left: 5px;
+    border-radius: 5px;
+    font-size: 1.5rem;
+    color: black;
+    width: 50%;
+    background: orange;
+    text-align: end;
   }
 `
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,45 +4,64 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <Nav>
-      <div className='title'>
-        <Link to='/' className='logo'>
-          TheCocktailBar
-        </Link>
-      </div>
-      <div>
-        <Link to='/' className='btn'>
-          Home
-        </Link>
-        <Link to='/about' className='btn'>
-          About
-        </Link>
-      </div>
-    </Nav>
+    <Wrapper>
+      <Nav>
+        <div className='title'>
+          <Link to='/' className='logo'>
+            <i>TheCocktailDB</i>
+          </Link>
+        </div>
+        <div>
+          <Link to='/' className='btn'>
+            Home
+          </Link>
+          <Link to='/about' className='btn'>
+            About
+          </Link>
+        </div>
+      </Nav>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 5rem;
+  background-color: #373737;
+  margin-bottom: 3.5rem;
+`
 
 const Nav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-bottom: 3.5rem;
+  width: 80vw;
+  max-width: 80vw;
+  margin: 0 auto;
+
   padding: 1.5rem;
-  background-color: #373737;
 
   .logo {
     font-size: 2.25rem;
     color: orange;
+    padding: 0.2rem 1rem;
+  }
+  .logo:hover {
+    background: orange;
+    color: #fcfcfc;
+    transition: 0.1s;
   }
   .btn {
     cursor: pointer;
     font-size: 1.5rem;
     padding: 0 0.5rem;
+    margin-right: 2.5rem;
     color: #fcfcfc;
   }
   .btn:hover {
-    color: #737373;
-    transition: 0.1;
+    background: orange;
+    border-radius: 5px;
+    transition: 0.1s;
   }
 `
 
