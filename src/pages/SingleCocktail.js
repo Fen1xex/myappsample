6c43d9ea862bc50ad230de6cb98d606ecabea89a
KIRILL F <tiltlessman@gmail.com>
2022-03-05 12:57:10 +0300

    update single drink page

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -12,6 +12,7 @@ const SingleCocktail = () => {
   const fetchSingleDrink = async (url) => {
     const response = await fetch(url)
     const data = await response.json()
+    console.log(data.drinks[0])
     setDrink(data.drinks[0])
   }
 
@@ -19,7 +20,27 @@ const SingleCocktail = () => {
     fetchSingleDrink(`${url}${id}`)
   }, [id])
 
-  const { strDrink: name, strDrinkThumb: img } = drink
+  const {
+    strDrink: name,
+    strDrinkThumb: img,
+    strCategory: category,
+    strAlcoholic: alcoholic,
+    strGlass: glass,
+    strInstructions: instructions,
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+  } = drink
+
+  const ingridients = [
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+  ]
 
   return (
     <>
@@ -27,7 +48,29 @@ const SingleCocktail = () => {
       <Wrapper>
         <div>
           <img src={img} alt={name} />
-          <h4>{name}</h4>
+          <div className='start'>
+            <h4>
+              <span>name:</span> {name}
+            </h4>
+            <h4>
+              <span>alcoholic:</span> {alcoholic}
+            </h4>
+            <h4>
+              <span>category:</span> {category}
+            </h4>
+            <h4>
+              <span>glass:</span> {glass}
+            </h4>
+            <p className='start'>
+              <span>ingridients: </span>
+              {ingridients.map((item, index) => {
+                return item ? <h5 key={index}>{item}</h5> : null
+              })}
+            </p>
+            <p>
+              <span>instruction:</span> {instructions}
+            </p>
+          </div>
         </div>
       </Wrapper>
     </>
@@ -38,7 +81,6 @@ const Wrapper = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 95vh;
   div {
     width: 350px;
     height: 350px;
@@ -47,6 +89,34 @@ const Wrapper = styled.section`
 
   img {
     width: 100%;
+    margin-bottom: 1.5rem;
+    border: 2px orange solid;
+    border-radius: 5px;
+  }
+
+  h4 {
+    font-weight: 100;
+    margin-bottom: 1.2rem;
+  }
+
+  h5 {
+    font-weight: 100;
+    font-size: 1rem;
+  }
+
+  span {
+    background-color: orange;
+    padding: 0.15rem 0.3rem;
+    color: #fcfcfc;
+    border-radius: 3px;
+  }
+
+  .start {
+    text-align: start;
+  }
+
+  p {
+    letter-spacing: 0.15rem;
   }
 `
 
